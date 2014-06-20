bbb95315ffb23399f48fb8cecc2e1992e8528649
Michael Sharman <sharmo@gmail.com>
2014-06-20 12:22:29 +1000

    [FEATURE] Added method to run recursive requests
    requestRecursive() was added to make recursive requests to the
    data api. You can either add the body of each request to a
    local variable that would be returned at the completion, however
    this isn't recommended.
    
    Instead you can pass a callback to requestRecursive() that is
    triggered on completion of each run of the Data API.

--- src/LearnositySdk/Request/DataApi.php
+++ src/LearnositySdk/Request/DataApi.php
@@ -2,8 +2,10 @@
 
 namespace LearnositySdk\Request;
 
+use \Exception;
 use LearnositySdk\Request\Init;
 use LearnositySdk\Request\Remote;
+use LearnositySdk\Utils\Json;
 
 /**
  *--------------------------------------------------------------------------
@@ -17,11 +19,63 @@ use LearnositySdk\Request\Remote;
 
 class DataApi
 {
-    public function request($url, $securityPacket, $secret, $requestPacket = null, $action = null)
+    private $remote;
+
+    public function __construct()
     {
-        $init   = new Init('data', $securityPacket, $secret, $requestPacket, $action);
+        $this->remote = new Remote();
+    }
+
+    /**
+     * Makes a single request to the data api
+     *
+     * @param  string  $endpoint       URL to send the request
+     * @param  array   $securityPacket Security details
+     * @param  string  $secret         Private key
+     * @param  array   $requestPacket  Request packet
+     * @param  string  $action         Action for the request
+     * @return Remote                  Instance of the Remote class
+     */
+    public function request($endpoint, $securityPacket, $secret, $requestPacket = null, $action = null)
+    {
+        $init = new Init('data', $securityPacket, $secret, $requestPacket, $action);
         $params = $init->generate();
-        $remote = new Remote();
-        return $remote->post($url, $params);
+        return $this->remote->post($endpoint, $params);
+    }
+
+    /**
+     * Makes a recursive request to the data api, dependant on
+     * whether 'next' is returned in the meta object
+     *
+     * @param  string  $endpoint       URL to send the request
+     * @param  array   $securityPacket Security details
+     * @param  string  $secret         Private key
+     * @param  array   $requestPacket  Request packet
+     * @param  string  $action         Action for the request
+     * @param  mixed   $callback       Optional callback to execute instead of returning data
+     * @return array                   Array of all data requests or [] or using a callback
+     */
+    public function requestRecursive($endpoint, $securityPacket, $secret, $requestPacket = null, $action = null, $callback = null)
+    {
+        $response = array();
+
+        do {
+            $request = $this->request($endpoint, $securityPacket, $secret, $requestPacket, $action);
+            $data = json_decode($request->getBody(), true);
+            if ($data['meta']['status'] === true) {
+                if (!empty($callback) && is_callable($callback)) {
+                    call_user_func($callback, $data);
+                } else {
+                    $response = array_merge($response, $data['data']);
+                }
+            } else {
+                throw new Exception(Json::encode($data));
+            }
+            if (array_key_exists('next', $data['meta'])) {
+                $requestPacket['next'] = $data['meta']['next'];
+            }
+        } while (array_key_exists('next', $data['meta']));
+
+        return $response;
     }
 }
