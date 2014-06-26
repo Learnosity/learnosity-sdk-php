2895c1d18fd68ce167a3b09ea5b097bda69e2131
Karol Tarasiuk <karol.tarasiuk@learnosity.com>
2014-06-26 11:28:02 +1000

    [REFACTOR] ValidationException added
    https://app.asana.com/0/12866594763058/13380782980474
    
    $data argument removed

--- src/LearnositySdk/Exceptions/ValidationException.php
+++ src/LearnositySdk/Exceptions/ValidationException.php
@@ -3,17 +3,5 @@ namespace LearnositySdk\Exceptions;
 
 class ValidationException extends \Exception
 {
-    public $data = null;
 
-    /**
-     * @param string        $message  exception message
-     * @param integer       $code     user defined exception code
-     * @param Exception     $previous previous exception if nested exception
-     * @param mixed         $data     data which caused this exception to raise
-     */
-    public function __construct($data = null, $message = '', $code = 0, $previous = null)
-    {
-        $this->data = $data;
-        parent::__construct($message, $code, $previous);
-    }
 }
